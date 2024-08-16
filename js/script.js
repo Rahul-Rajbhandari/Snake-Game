bd709192a69f0b665219b07c70e6ea604c9553eb
Rahul <rahulrajbhandari19@gmail.com>
2024-08-16 23:11:26 +0545

    collision detection and food added

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 //Game Constants and Variables
-let direction = { x: 0, y: 0 };
+let inputDirection = { x: 0, y: 0 };
 const foodSound = new Audio('../Sounds/food.mp3');
 const gameOverSound = new Audio('../Sounds/gameover.mp3');
 const moveSound = new Audio('../Sounds/move.mp3');
 const musicSound = new Audio('../Sounds/music.mp3');
+let score = 0;
 let speed = 2;
 let lastPaintTime = 0;
 let snakeArr = [
@@ -22,9 +23,29 @@ function main(ctime) {
     gameEngine();
 }
 
+function isCollide(sarr){
+    return false;
+}
+
 function gameEngine() {
     //Part 1: Updating the snake array
+    if (isCollide(snakeArr)){
+        gameOverSound.play;
+        musicSound.pause();
+        inputDirection = {x:0 ,y:0}  
+        alert("Game Over Press any key to play again");
+        snakeArr = [ { x: 13, y: 15 }]
+        musicSound.play();
+        score = 0;
+    }
 
+    //If you have eaten the food, increment the score and regenerate the food
+    if (snakeArr[0].y ==food.y && snakeArr[0].x == food.x){
+        snakeArr.unshift({x: snakeArr[0].x + inputDirection.x, y: snakeArr[0].y + inputDirection.y})
+        let a = 2;
+        let b = 16;
+        food = {x: Math.round(a+(b-a)*Math.random()), y: Math.round(a+(b-a)*Math.random())}
+    }
 
     //Part 2: Render the snake and Food
     // Display the snake 
@@ -62,15 +83,23 @@ window.addEventListener('keydown',e =>{
     switch(e.key){
         case "ArrowUp":
             console.log("ArrowUp");
+            inputDirection.x = 0;
+            inputDirection.y = -1;
             break;
         case "ArrowDown":
             console.log("ArrowDown");
+            inputDirection.x = 0;
+            inputDirection.y = 1;
             break;
         case "ArrowLeft":
             console.log("ArrowLeft");
+            inputDirection.x = -1;
+            inputDirection.y = 0;
             break;
         case "ArrowRight":
             console.log("ArrowRight");
+            inputDirection.x = 1;
+            inputDirection.y = 0;
             break;
 
         default: 
